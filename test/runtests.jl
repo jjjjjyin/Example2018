ca6b8a592635716ab0da71b19435aee79096fbc0
Junjie <jjjjjyin@gmail.com>
2018-09-21 14:42:41 -0400

    test some ring properties

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,25 @@
+using Test
 using Example2018
 
-@show Polynomial([1,2,3])
-@show evaluate(Polynomial([1,2,3]), 2)
-@show Polynomial([1,2,3]) + Polynomial([4,5])
-@show 3 * Polynomial([1,2,4,42])
-@show deriv(Polynomial([1,1,1,1,1]))
-@show deriv(Polynomial([0,0,1,0,0]))
+# Polynomials form a ring
+# 1. addition is associatice
+# 2. additive identity
+# 3. additice inverse
+# 4. addition commutes
+# 5. multiplication is ...
+
+@testset "Polynomial form a ring" begin
+
+	z = Polynomial([0])
+
+	for n in 1:100
+
+	deg = rand(0:100)
+		p = Polynomial(rand(-1000000:1000000, deg+1))
+
+		@test p + z == p
+		@test z + p == p
+		@test p + (-p) == z
+	end
+
+end
